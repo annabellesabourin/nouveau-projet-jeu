c99e0f285cfdbc440c59da7a5f7eff795dc143b2
annabellesabourin <annabelle.sabourin@icloud.com>
2026-03-18 12:55:41 -0400

    player réparé

--- Assets/controllerdialogue.cs
+++ Assets/controllerdialogue.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class dialoguecontrollerrr : MonoBehaviour
+public class controllerdialogue : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
